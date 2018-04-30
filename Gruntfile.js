c751f009a78832affe8474c0172be825885a3237
Matthew Cranford <mcranfordmail@gmail.com>
2018-04-30 16:18:52 -0700

    Fix images

--- Gruntfile.js
+++ Gruntfile.js
@@ -14,10 +14,10 @@ module.exports = function(grunt) {
         options: {
           // engine: 'im',
           sizes: [{
-            
+        
             // Change these:
-            width: 1600,
-            suffix: '_large_x2',
+            width: 500,
+            suffix: 'small',
             quality: 30
             
           }]
@@ -35,39 +35,12 @@ module.exports = function(grunt) {
         }]
       }
     },
-
-    /* Clear out the images directory if it exists */
-    clean: {
-      dev: {
-        src: ['images'],
-      },
-    },
-
-    /* Generate the images directory if it is missing */
-    mkdir: {
-      dev: {
-        options: {
-          create: ['images']
-        },
-      },
-    },
-
-    /* Copy the "fixed" images that don't go through processing into the images/directory */
-    copy: {
-      dev: {
-        files: [{
-          expand: true,
-          src: 'images_src/fixed/*.{gif,jpg,png}',
-          dest: 'images/'
-        }]
-      },
-    },
   });
   
   grunt.loadNpmTasks('grunt-responsive-images');
-  grunt.loadNpmTasks('grunt-contrib-clean');
-  grunt.loadNpmTasks('grunt-contrib-copy');
-  grunt.loadNpmTasks('grunt-mkdir');
-  grunt.registerTask('default', ['clean', 'mkdir', 'copy', 'responsive_images']);
+  // grunt.loadNpmTasks('grunt-contrib-clean');
+  // grunt.loadNpmTasks('grunt-contrib-copy');
+  // grunt.loadNpmTasks('grunt-mkdir');
+  grunt.registerTask('default', ['responsive_images']);
 
 };
